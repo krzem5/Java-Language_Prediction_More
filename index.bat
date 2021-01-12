329b18d181ed72b89c1271c46df91e586ce1b940
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:14:29 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/language_prediction_more/Main.java&&java com/krzem/language_prediction_more/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/language_prediction_more/Main.java&&jar cvmf ../manifest.mf ../build/language_prediction_more.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="language_prediction_more.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/language_prediction_more.jar
+:end
