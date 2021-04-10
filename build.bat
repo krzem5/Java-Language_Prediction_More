fcc6a8eb69b757e954154588e554576969ee2e0c
Krzem <krzem5.dev@gmail.com>
2021-04-10 09:36:54 +0200

    Push Update 04/10/2021, 09:36:35

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/language_prediction_more/Main.java&&jar cvmf ../manifest.mf ../build/language_prediction_more.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="language_prediction_more.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/language_prediction_more.jar
-:end
+python build.py %*
